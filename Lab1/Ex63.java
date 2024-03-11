8541840b1c4a9806c6181a85978c7743084d5463
HuuHoang2707 <Hoang.NH225972@sis.hust.edu.vn>
2024-03-11 11:37:07 +0700

    HW Lab1

---
+++ Lab1/Ex63.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex63 {
+
+    static void printStar(int n) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= 2*n-1; ++j) {
+                if (j >= n-i+1 && j <= n + i - 1)
+                    System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        
+        printStar(t);
+    }
+}
